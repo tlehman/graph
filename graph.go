40e16c39cb703c8c69029b4aa35ceec6d62ba356
Tobi Lehman <mail@tobilehman.com>
2016-03-23 21:36:37 -0700

    Add component search using BFS

--- graph.go
+++ graph.go
@@ -3,9 +3,10 @@ package graph
 import (
 	"bytes"
 	"fmt"
+	"github.com/tlehman/ds"
 )
 
-// Each edge has a destination (y), a weight (optional) and
+
 // forms a linked list of incident edges
 type edge struct {
 	y      int
@@ -45,9 +46,10 @@ func (g AdjList) EdgeCount() int {
 	return g.edgeCount
 }
 
+
 // Add edge adds an edge node to the x-th spot in the edges slice.
 // It resizes if necessary.
-func (g *AdjList) AddEdge(x, y int) {
+func (g *AdjList) addEdge(x, y int) {
 	if max(x,y) >= len(g.edges) {
 		g.resizeEdges(max(x,y))
 	}
@@ -59,9 +61,6 @@ func (g *AdjList) AddEdge(x, y int) {
 		g.edges[x] = &edge{y: y, next: newnext}
 	}
 	g.edgeCount += 1
-	if !g.directed && g.edges[y].y != x {
-		g.AddEdge(y, x)
-	}
 	// record the vertices
 	if !g.vertexset[x] {
 		g.vertexset[x] = true
@@ -70,6 +69,13 @@ func (g *AdjList) AddEdge(x, y int) {
 	if !g.vertexset[y] {
 		g.vertexset[y] = true
 		g.vertexCount += 1
+	}
+}
+
+func (g *AdjList) AddEdge(x, y int) {
+	g.addEdge(x,y)
+	if !g.directed {
+		g.addEdge(y,x)
 	}
 }
 
@@ -84,16 +90,75 @@ func (g *AdjList) resizeEdges(size int) {
 // Builds a graphviz string representing the graph 
 func (g AdjList) String() string {
 	var buffer bytes.Buffer 
-	buffer.WriteString("digraph {\n")
+	var arrow string
+	if g.directed {
+	    buffer.WriteString("di")
+		arrow = "->"
+	} else {
+		arrow = "--"
+	}
+	buffer.WriteString("graph {\n")
 	for x, e := range g.edges {
 		if x > -1 && e.y > -1 {
 			for c := e; c != nil; c = c.next {
-				buffer.WriteString(fmt.Sprintf("  %d -> %d;\n", x, c.y))
+				buffer.WriteString(fmt.Sprintf("  %d %s %d;\n", x, arrow, c.y))
 			}
 		}
 	}
 	buffer.WriteString("}")
 	return buffer.String()
+}
+
+// Find connected components in an undirected graph.
+// The return value of Components() is a slice of integers
+// that associates each vertex with a component number.
+func (g *AdjList) Components() []int {
+	// each index v is a vertex, and the value comps[v]
+	// is the number of the component v is in
+	comps := make([]int, len(g.edges))
+	discovered := make([]bool, len(g.edges))
+	compNum := 0
+	for v, _ := range g.edges {
+		if !discovered[v] {
+			// indices of component containing v
+			idcsComp := g.bfs(v)
+			for _, x := range idcsComp {
+				comps[x] = compNum
+				discovered[x] = true
+			}
+			compNum++
+		}
+	}
+
+	return comps
+}
+
+
+const (
+	undiscovered = iota
+	discovered = iota
+	processed = iota
+)
+	
+// do a Breadth-First Search from v, return indices in same
+// component as v
+func (g *AdjList) bfs(v int) []int {
+	state := make([]int, len(g.edges)) // all initially undiscovered
+	indices := make([]int, 0)
+	q := ds.NewQueue()
+	q.Enqueue(v)
+	for q.Len() > 0 {
+		x := q.Dequeue().(int)
+		indices = append(indices, x)
+		for e := g.edges[x]; e != nil; e = e.next {
+			if e.y > -1 && state[e.y] == undiscovered {
+				state[x] = discovered 
+				q.Enqueue(e.y)
+			}
+		}
+		state[x] = processed
+	}
+	return indices
 }
 
 func max(x, y int) int {
--- graph_test.go
+++ graph_test.go
@@ -1,11 +1,10 @@
 package graph
 
 import (
-	"fmt"
 	"testing"
 )
 
-func graphSixVertices() AdjList {
+func digraphSixVertices() AdjList {
 	directed := true
 	g := New(directed)
 	g.AddEdge(0, 2)
@@ -20,7 +19,7 @@ func graphSixVertices() AdjList {
 }
 
 func TestAddEdge(t *testing.T) {
-	g := graphSixVertices()
+	g := digraphSixVertices()
 	if g.VertexCount() != 6 {
 		t.Fatalf("Expected vertex count to be 6, got %d", g.VertexCount())
 	}
@@ -30,7 +29,7 @@ func TestAddEdge(t *testing.T) {
 }
 
 func TestString(t *testing.T) {
-	g := graphSixVertices()
+	g := digraphSixVertices()
 	expected := `digraph {
   0 -> 5;
   0 -> 2;
@@ -42,9 +41,70 @@ func TestString(t *testing.T) {
   5 -> 2;
 }`
 	actual := g.String()
-	fmt.Println(actual)
 	if actual != expected {
 		t.Fatal("Graphviz representation not working correctly")
 		t.Fatalf("actual result: \n%s\n", actual)
+	}
+}
+
+func graphSevenVertices() AdjList {
+	directed := false
+	g := New(directed)
+	g.AddEdge(0, 2)
+	g.AddEdge(1, 2)
+	g.AddEdge(1, 3)
+	g.AddEdge(2, 3)
+
+	g.AddEdge(4, 5)
+	g.AddEdge(5, 6)
+	g.AddEdge(6, 4)
+	return g
+}
+
+func TestStringUndirected(t *testing.T) {
+	var g AdjList = graphSevenVertices()
+	expected := `graph {
+  0 -- 2;
+  1 -- 3;
+  1 -- 2;
+  2 -- 3;
+  2 -- 1;
+  2 -- 0;
+  3 -- 2;
+  3 -- 1;
+  4 -- 6;
+  4 -- 5;
+  5 -- 6;
+  5 -- 4;
+  6 -- 4;
+  6 -- 5;
+}`
+	actual := g.String()
+	if actual != expected {
+		t.Fatal("Graphviz representation not working correctly")
+		t.Fatalf("actual result: \n%s\n", actual)
+	}
+}
+
+// Find connected components in an undirected graph.
+// The return value of Components() is a slice of integers
+// that associates each vertex with a component number.
+func TestComponents(t *testing.T) {
+	var g AdjList = graphSevenVertices()
+	var comps []int = g.Components()
+	var compOne bool = true
+	var compTwo bool = true
+	for i := 0; i < 4; i++ {
+		if comps[i] != 0 {
+			compOne = false
+		}
+	}
+	for i := 4; i < 7; i++ {
+		if comps[i] != 1 {
+			compTwo = false
+		}
+	}
+	if !compOne || !compTwo {
+		t.Fatalf("%v", comps)
 	}
 }
